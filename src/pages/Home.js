610ca8665248792aa5424b898c9f3a685fd3fa4e
Muizz Ahmed <muizz.ahmed323@gmail.com>
2021-11-27 17:04:36 -0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,13 @@ import ScrollAnimation from 'react-animate-on-scroll';
 
 
 function Home() {
+    const wow = prompt('Please enter your name')
     return (
       <div className="bodyContainer" >
         <div className="homeContainer">
           <ScrollAnimation animateIn="animate__fadeInDown" animateOnce={true} delay={100}>
             <h1 className="whiteText">Bee Better</h1>
+            {wow}
           </ScrollAnimation>
           <ScrollAnimation animateIn="animate__fadeInDown" animateOnce={true} delay={500}>
             <h5 className="whiteText">An App to help Track your Personal Growth!!</h5>
